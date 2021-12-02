869f9949af3a71e24e209c3fbb3436319fea4475
Stefano Allegra <stefano@qodeup.com>
2021-12-02 18:00:45 +0100

    Set up the app only with the total amount

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { Header } from './modules/header';
-import { Opening } from './modules/opening-bill';
 import { TotalAmount } from './modules/total-amount';
-import { SplitTheBill } from './modules/split-the-bill';
-import { Tips } from './modules/tips';
-import { Checkout } from './modules/checkout';
-import { ThankYou } from './modules/thank-you';
-import { PaymentError } from './modules/payment-error';
-import { AlreadyPaid } from './modules/already-paid';
 
 import './app.css';
 
@@ -16,13 +9,6 @@ function App() {
     <div className="App">
       <Header />
       <TotalAmount />
-      <SplitTheBill />
-      <Tips />
-      <Checkout />
-      {/* <Opening /> */}
-      {/* <ThankYou /> */}
-      {/* <PaymentError /> */}
-      {/* <AlreadyPaid /> */}
     </div>
   );
 }
