63c75dc10bc6a94ac0cff780e04615ef36299e9e
igor <igor.topic@gmail.com>
2021-12-03 15:58:35 +0100

    code challenge

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Header } from './modules/header';
-import { TotalAmount } from './modules/total-amount';
+import React from "react";
+import { Header } from "./modules/header";
+import { TotalAmount } from "./modules/total-amount";
+// eslint-disable-next-line no-use-before-define
+import socketClient from "socket.io-client";
 
-import './app.css';
+import "./app.css";
+const SERVER = "http://127.0.0.1:8080";
 
 function App() {
+  var socket = socketClient(SERVER);
+  socket.on("connection", () => {
+    console.log(`I'm connected with the back-end`);
+  });
+  socket.on("updateAmount", (data) => {
+    console.log(`Updated Amount ==>`, data);
+  });
   return (
     <div className="App">
       <Header />
--- src/modules/total-amount/index.tsx
+++ src/modules/total-amount/index.tsx
@@ -1,44 +1,73 @@
-import React, { ComponentProps, FC, PropsWithChildren, ReactElement } from "react";
+import React, { ComponentProps, FC, PropsWithChildren, ReactElement, useEffect } from "react";
 import './total-amount.css';
+// eslint-disable-next-line no-use-before-define
+import socketClient from "socket.io-client";
+const SERVER = "http://127.0.0.1:8080";
 
 export interface HeaderProps extends PropsWithChildren<{}> { };
+
+interface ContentProps {
+    totalAmount?: any;
+    paidAmount?: any;
+    remainingAmount?: any;
+}
 
 export const Wrapper: FC<HeaderProps> = (props: HeaderProps) =>
     <section id="total-amount">{props.children}</section>
 
-export const Content: FC<HeaderProps> = ({ }) => (
-    <>
-        <div className="title">
-            total bill
-        </div>
-        <div className="amount">
-            --
-        </div>
-        <div className="already-paid mt-40">
-            <div className="row">
-                <div className="col-6 t-left">
-                    Already paid
-                </div>
-                <div className="col-6 t-right qodeup-color">
-                    0<small>.00€</small>
-                </div>
+export const Content: FC<ContentProps> = (props: ContentProps) => {
+    return (
+        <>
+            <div className="title">
+                total bill
             </div>
-            <div className="row mt-10">
-                <div className="col-6 t-left">
-                    Remaining to pay
-                </div>
-                <div className="col-6 t-right qodeup-color">
-                    0<small>.00€</small>
-                </div>
+            <div className="amount">
+                {/* {} */}
+                {props.remainingAmount !== 0 ? props.totalAmount : 'BILL PAID Thank you'}
             </div>
-        </div>
-    </>
-)
+            <div className="already-paid mt-40">
+                <div className="row">
+                    {props.paidAmount !== 0 ?
+                    <> <div className="col-6 t-left">
+                        Already paid
+                    </div>
+                    <div className="col-6 t-right qodeup-color">
+                        {props.paidAmount}<small>.00€</small>
+                    </div></> : <></>}
+                </div>
+                {props.remainingAmount !== 0 ? <div className="row mt-10">
+                    <div className="col-6 t-left">
+                        Remaining to pay
+                    </div>
+                    <div className="col-6 t-right qodeup-color">
+                        {props.remainingAmount}<small>.00€</small>
+                    </div>
+                </div> : <div></div>}
+                
+            </div>
+        </>
+    );
+}
+
 
 export const TotalAmount: FC<HeaderProps> = ({ }) => {
+    const [totalAmount, setTotalAmount] = React.useState('')
+    const [paidAmount, setPaidAmount] = React.useState('')
+    const [remainingAmount, setRemainingAmount] = React.useState('')
+    var socket = socketClient(SERVER);
+    useEffect(() => {
+        socket.on('totalAmount', (obj) => {
+            if (obj.amount !== totalAmount) setTotalAmount(obj.amount);
+        });
+        socket.on('updateAmount', (obj) => {
+            if (obj.paid !== paidAmount) setPaidAmount(obj.paid);
+            if (obj.remaining !== remainingAmount) setRemainingAmount(obj.remaining);
+        })
+    })
+    
     return (
         <Wrapper>
-            <Content />
+            <Content totalAmount={totalAmount} paidAmount={paidAmount} remainingAmount={remainingAmount}  />
         </Wrapper>
     )
 };
